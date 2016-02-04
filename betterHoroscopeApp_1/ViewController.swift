bba25d99d494ac13289e6940e1e75e5e773c6cdb
Lab User <paperboy@arch-120-e10.lab.circa.ufl.edu>
2016-02-04 03:46:22 -0500

    Initial Commit

---
+++ betterHoroscopeApp_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  betterHoroscopeApp_1
+//
+//  Created by Aden, Naheen on 2/4/16.
+//  Copyright © 2016 adenLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
